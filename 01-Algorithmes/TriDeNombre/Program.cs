5ebffe21adb224ec9103b90ebd1d20441ad71e75
LucasLemmel <148881073+LucasLemmel@users.noreply.github.com>
2024-09-24 10:14:12 +0200

    Fin TriDeNombre

--- 01-Algorithmes/TriDeNombre/Program.cs
+++ 01-Algorithmes/TriDeNombre/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//VARIABLES
+
+int A;
+int B;
+int C;
+int[]nombres;
+string saisieUtilisateur;
+
+//TRAITEMENT
+
+Console.WriteLine("Entrez le premier nombre");
+saisieUtilisateur = Console.ReadLine();
+A = int.Parse(saisieUtilisateur);
+
+Console.WriteLine("Entrez le deuxième nombre");
+saisieUtilisateur = Console.ReadLine();
+B = int.Parse(saisieUtilisateur);
+
+Console.WriteLine("Entrez le troisième nombre");
+saisieUtilisateur = Console.ReadLine();
+C = int.Parse(saisieUtilisateur);
+
+nombres = [A,B,C];
+
+Array.Sort(nombres);
+
+//AFFICHAGE
+
+for (int i = 0;i < nombres.Length;i++)
+{
+    Console.WriteLine(nombres[i]);
+}
+Console.ReadLine();
