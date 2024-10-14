29df3f20f69670a994942a714a28e8bed4eb2a7c
LucasLemmel <148881073+LucasLemmel@users.noreply.github.com>
2024-10-14 08:39:05 +0200

    modofication projet tri de nombre

--- 01-Algorithmes/TriDeNombre/Program.cs
+++ 01-Algorithmes/TriDeNombre/Program.cs
@@ -1,16 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LLemmelTools;
 Console.WriteLine("Hello, World!");
 
 //VARIABLES
 
-int A;
-int B;
-int C;
+int A = ConsoleTools.DemanderNombreEntier("Entez le premier nombre");
+int B = ConsoleTools.DemanderNombreEntier("Entez le deuxième nombre");
+int C = ConsoleTools.DemanderNombreEntier("Entez le troisième nombre");
 int[]nombres;
 string saisieUtilisateur;
 
 //TRAITEMENT
-
+/*
 Console.WriteLine("Entrez le premier nombre");
 saisieUtilisateur = Console.ReadLine();
 A = int.Parse(saisieUtilisateur);
@@ -22,6 +23,7 @@ B = int.Parse(saisieUtilisateur);
 Console.WriteLine("Entrez le troisième nombre");
 saisieUtilisateur = Console.ReadLine();
 C = int.Parse(saisieUtilisateur);
+*/
 
 nombres = [A,B,C];
 
