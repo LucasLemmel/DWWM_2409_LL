0f12f452c3db80fe825ceb911e040e1a931659b4
LucasLemmel <148881073+LucasLemmel@users.noreply.github.com>
2025-03-11 16:53:58 +0100

    realisation exo javascript

---
+++ 02-Webfront/Javascript/codePostale/assets/script.js
@@ -0,0 +1,40 @@
+fetch("https://arfp.github.io/tp/web/javascript/02-zipcodes/zipcodes.json")
+.then(Response=>{
+    return Response.json();
+}
+).then(data=>{
+    console.log(data);
+    genererListe(data);
+   
+    
+
+})
+.catch(error => alert("Erreur : " + error));
+
+
+
+
+let myData =[];
+function genererListe(_data) {
+    myData = _data;
+    
+    for (let i = 0; i < _data.length; i++) {
+        let myOption = new Option();
+        myOption.value = _data[i].codeCommune;
+        myOption.textContent = _data[i].nomCommune + " cp:" + _data[i].codePostal;
+
+       document.getElementById("list-cp").appendChild(myOption);
+    };
+};
+
+const btnValider = document.querySelector("#btn-choix");
+    btnValider.addEventListener("click",function () {
+    let selection = document.getElementById("list-commune").value;
+    console.log(selection);
+    let maVille = myData.find(item=>item.nomCommune == selection);
+    const information = document.querySelector("#info");  
+    
+    console.log(maVille);
+
+    information.innerHTML ="Code postal: "+ maVille.codePostal +"<br>Commune: "+ maVille.nomCommune +"<br>Acheminement: "+ maVille.libelleAcheminement;
+});
