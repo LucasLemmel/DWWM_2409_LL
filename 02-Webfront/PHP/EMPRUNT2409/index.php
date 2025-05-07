2b324eaa193ea2a20623f518134b0d3af7dcf442
LEMMEL Lucas <LLemmel@arfp.asso.fr>
2025-05-07 11:47:54 +0200

    exo php

---
+++ 02-Webfront/PHP/EMPRUNT2409/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Affichage tableau amortissement Prêt</title>
+</head>
+<body>
+<?php
+include "./models/Pret.php";
+
+$objPret= new Pret(10000, 5.3,5);
+
+echo $objPret->calculMensualite2()."<br>";
+
+$tableauAmrt= $objPret->getTableauAmortissement();
+var_export($tableauAmrt);
+
+
+?>
+</body>
+</html>
